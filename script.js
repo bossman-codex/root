c785dc385641ed9f1c52822b33257e5012af42f7
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2021-04-22 08:57:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ const database = knex({
   app.use(bodyParser.json())
   app.use(cors())
 
-//   app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//  })
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 
 
 
